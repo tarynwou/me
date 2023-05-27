40552779b194c85014d5c115d05a1787d2d3f01e
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-05-27 15:57:26 -0700

    created tester ExperienceCard

--- nextjs-blog/components/Experience.tsx
+++ nextjs-blog/components/Experience.tsx
@@ -1,14 +1,24 @@
 import styles from "../styles/sections.module.css";
 import { Stack } from "@mui/material";
 import ExperienceToggle from "./ExperienceToggle";
+import ExperienceCard from "./ExperienceCard";
 
 export default function Experience() {
 	return (
-		<Stack>
+		<Stack className={styles.experienceSection}>
 			<h1 className={styles.sectionHeader}>Experience</h1>
 			<div className={styles.experienceToggleWrapper}>
 				<ExperienceToggle></ExperienceToggle>
 			</div>
+			<ExperienceCard
+				imgSrc="/images/taryn-wou-headshot.png"
+				experienceOrg="Copilot AI"
+				experienceRole="Associate Product Manager Intern"
+				experienceLocation="Vancouver, BC"
+				experienceStartDate="May 2023"
+				experienceEndDate="Present"
+				experienceDescription="Co-own cross functional team OKRs with the support of design, engineering and marketing counter parts, Research & conduct extensive customer interviews and data analysis to get better understanding of company customer base, Co-own the acceptance criteria with the Product Manager, Product Designer and Engineering counterparts to ensure quality, Create solid user stories that will enable successful engineering delivery"
+			></ExperienceCard>
 		</Stack>
 	);
 }
---
+++ nextjs-blog/components/ExperienceCard.tsx
@@ -0,0 +1,39 @@
+import styles from "../styles/sections.module.css";
+import { Stack } from "@mui/material";
+
+export default function ExperienceCard(props) {
+	return (
+		<Stack className={styles.experienceCard} direction="row">
+			<div className={styles.experienceImageContainer}>
+				<img
+					className={styles.experienceImage}
+					src={props.imgSrc}
+					alt={props.experienceOrg}
+				/>
+			</div>
+
+			<Stack>
+				<h2 className={styles.experienceRole}>
+					{props.experienceRole}
+				</h2>
+				<Stack direction="row">
+					<h3 className={styles.experienceOrg}>
+						{props.experienceOrg}
+					</h3>
+					|
+					<h3 className={styles.experienceLocation}>
+						{props.experienceLocation}
+					</h3>
+				</Stack>
+
+				<h3 className={styles.experienceDate}>
+					{props.experienceStartDate} – {props.experienceEndDate}
+				</h3>
+
+				<p className={styles.experienceDescription}>
+					{props.experienceDescription}
+				</p>
+			</Stack>
+		</Stack>
+	);
+}
