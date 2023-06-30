e748bd2f4515a8df18f6220adda19706c33e3375
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2023-06-30 15:17:11 -0700

    updated contact button styles

--- nextjs-blog/components/Connect.tsx
+++ nextjs-blog/components/Connect.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import styles from "../styles/sections.module.css";
 import { Stack } from "@mui/material";
-import Button from "@mui/material/Button";
 import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import { makeStyles, Theme } from "@mui/material/styles";
-
-// const useStyles = makeStyles((theme: Theme) => ({
-// 	buttonLabel: {
-// 	  marginLeft: theme.spacing(1), // Adjust the spacing as needed
-// 	},
-//   }));
 
 export default function Connect() {
-	// const classes = useStyles();
-
 	return (
 		<>
 			<h1 className={styles.sectionHeader} id="connect">
@@ -24,46 +14,44 @@ export default function Connect() {
 			</h1>
 			<div className={styles.contactSection}>
 				<Stack className={styles.contactRow} direction="row">
-					<Button
+					<a
 						className={styles.contactButton}
-						variant="outlined"
-						startIcon={<EmailRoundedIcon />}
 						href="mailto:tarynwou@gmail.com"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
+						<EmailRoundedIcon className={styles.contactIcon} />
 						Email
-					</Button>
-
-					<Button
+					</a>
+					<a
 						className={styles.contactButton}
-						variant="outlined"
-						startIcon={<LinkedInIcon />}
 						href="https://www.linkedin.com/in/tarynwou/"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
+						<LinkedInIcon className={styles.contactIcon} />
 						LinkedIn
-					</Button>
+					</a>
 				</Stack>
 				<Stack className={styles.contactRow} direction="row">
-					<Button
+					<a
 						className={styles.contactButton}
-						variant="outlined"
-						startIcon={<GitHubIcon />}
 						href="https://github.com/tarynwou"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
+						<GitHubIcon className={styles.contactIcon} />
 						GitHub
-					</Button>
-
-					<Button
+					</a>
+					<a
 						className={styles.contactButton}
-						variant="outlined"
-						startIcon={<InstagramIcon />}
 						href="https://www.instagram.com/tarynwou/"
 						target="_blank"
+						rel="noopener noreferrer"
 					>
+						<InstagramIcon className={styles.contactIcon} />
 						Instagram
-					</Button>
+					</a>
 				</Stack>
 			</div>
 		</>
